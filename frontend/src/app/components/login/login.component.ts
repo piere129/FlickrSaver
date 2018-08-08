5b16259a6604b02e9a2bc47dca247820c48cc203
Pieter Desseyn <pieterdesseyn@gmail.com>
2018-08-08 22:25:38 +0200

    added login/logout functionality, authtoken saved in localstorage and deleted on logout. Navbar edits based upon logged in/logged out

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  password: string;
+
+  constructor(private flashMessagesService: FlashMessagesService,
+    private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      if (data.success) {
+        this.flashMessagesService.show('You have succesfully logged in!', {cssClass: 'alert-success', timeout: 3000});
+
+        this.authService.storeUserData(data.token, data.user);
+        this.router.navigate(['dashboard']);
+      } else {
+        this.flashMessagesService.show(data.msg, {cssClass: 'alert-danger', timeout: 3000});
+      }
+    });
   }
 
 }
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private flashMessagesService: FlashMessagesService,
+    private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessagesService.show('You are now logged out!', {cssClass: 'alert-success', timeout: 3000});
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+  isLoggedIn() {
+     return this.authService.isLoggedIn();
+  }
 }
